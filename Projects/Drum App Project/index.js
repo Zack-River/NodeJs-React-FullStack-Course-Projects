28fe266471a4d11887c5ee321e4ce4555caeda1f
Abdallah Wageeh(Zack River) <111385589+Zack-River@users.noreply.github.com>
2025-03-02 04:21:29 +0200

    18_ Advanced JS and DOM Manipulation
    18.0 EventListeners.
    18.1 Drum Project:
    -Higher Order Functions.
    -CallBack Functions.
    -passing functions as arguments.
    -audio element/tag html.
    -audio class in JS and play function.
    -switch statement.
    -dot notation for properties and methods of an object.
    -keydown listener and event.key property.
    -simple animation(classList.add/remove).
    -understanding callback in JS.
    
    *Project Description:
    Drum project is a webapp that works via mouse clicks or keyboard keys to play a musical sound of an instrument.

--- Projects/Drum App Project/index.js
+++ Projects/Drum App Project/index.js
@@ -1,7 +1,99 @@
 var btns = document.querySelectorAll(".drum");
-for(let i = 0; i < btns.length; i++)
-    btns[i].addEventListener("click", handleClick);
 
-function handleClick(){
-    alert("I Got Clicked!");
-}
+//detecting Mouse Clicks
+for (let i = 0; i < btns.length; i++)
+    btns[i].addEventListener("click", function () {
+        this.style.color = "white";
+        let buttonContent = btns[i].innerHTML;
+        makeSound(buttonContent);
+        buttonAnimation(buttonContent);
+    });
+
+    //detecting Keyboard Press
+    document.addEventListener("keydown", function(event) {
+        makeSound(event.key)
+        buttonAnimation(event.key)
+    });
+
+    function makeSound (character) {
+        switch (character) {
+            case "w":
+                var tom1 = new Audio("./sounds/tom-1.mp3");
+                tom1.play();
+                break;
+
+            case "a":
+                var tom2 = new Audio("./sounds/tom-2.mp3");
+                tom2.play();
+                break;
+
+            case "s":
+                var tom3 = new Audio("./sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+            case "d":
+                var tom4 = new Audio("./sounds/tom-4.mp3");
+                tom4.play();
+                break;
+
+            case "j":
+                var crash = new Audio("./sounds/crash.mp3");
+                crash.play();
+                break;
+            
+            case "k":
+                var kick = new Audio("./sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            
+            case "l":
+                var snare = new Audio("./sounds/snare.mp3");
+                snare.play();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    function buttonAnimation(currentKey) {
+        var activeButton = document.querySelector("." + currentKey);
+        activeButton.classList.add("pressed");
+        setTimeout(function(){removeAnimation(activeButton)}, 100);
+    }
+
+    function removeAnimation(element)
+    {
+        element.classList.remove("pressed");
+    }
+
+    var houseKeeper = {
+        name: "7amo",
+        YOE: 16,
+        cleaningRepetoire: ["bedrooms","living rooms"],
+    }
+
+    alert("Hello I am "+houseKeeper.name+"\n I am only an object.");
+
+    function HouseKeeper (name, YOE, cleaningRepetoire) {
+        this.name = name,
+        this.YOE = YOE,
+        this.cleaningRepetoire = cleaningRepetoire,
+        this.clean = function() {
+            alert("Cleaning In Progress....");
+        },
+        this.info = function() {
+            alert("Hello I am: "+this.name+".\nI have: "+this.YOE+" Years Of Experience.\n I am object of a class.");
+        }
+    }
+
+    var houseKeeper1 = new HouseKeeper("Ahmed",10,["stairs","rooms","bathrooms"]);
+    console.log(houseKeeper1.name);
+    houseKeeper1.info();
+    houseKeeper1.clean();
+
+    // call back functions are fucnctions which called inside higher order functions.
+    // document.addEverntListener("keypress", function());
+    // at this case addEverntListener() is higher order.
+    // while function() is a callback function.
