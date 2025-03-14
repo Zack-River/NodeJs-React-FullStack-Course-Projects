bf8c77a87cb4c12639a94da49bb80563a86994c3
Abdallah Wageeh(Zack River) <111385589+Zack-River@users.noreply.github.com>
2025-03-14 05:43:54 +0200

    26_ Capstone Blog Project Pt2
    -HomePage Implemented.
    -Edit still in Coding.

--- Backend/26_ Capstone Project Web App/public/js/index.js
+++ Backend/26_ Capstone Project Web App/public/js/index.js
@@ -15,3 +15,30 @@ document.addEventListener("click", (event) => {
 createButton.addEventListener("click" , () => {
     window.location.href = "/create";
 });
+
+document.querySelectorAll(".close-btn").forEach(button => {
+    button.addEventListener("click", function() {
+        const postRemove = this.closest(".post"); // Find the parent .post div
+        if (postRemove) {
+            postRemove.remove();
+            if(!document.querySelector(".post")) {
+                document.querySelector("main").innerHTML = `<h1 class="no-more">No More Posts To View</h1>`;
+            }
+        }
+    });
+});
+
+document.querySelector(".search").addEventListener("input", (event) => {
+    const searchValue = event.target.value.toLowerCase();
+    const posts = document.querySelectorAll(".post");
+    posts.forEach(post => {
+        const title = post.querySelector(".title").textContent.toLocaleLowerCase();
+        
+        if(title.includes(searchValue)) {
+            post.style.display = "flex";
+        }
+        else {
+            post.style.display = "none";
+        }
+    });
+});
